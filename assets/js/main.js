cf19688b53f78ccc671494d8da80b5dac2d769c8
pepellin <lin.2049@osu.edu>
2016-05-27 16:41:58 -0400

    carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,18 @@ $(function() {
       }
     });
   });
+  
+function initMap() {
+  var myLatLng = {lat: 40.02, lng: -83.02};
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: myLatLng,
+    zoom: 12
+  });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'The Ohio State University'
+      });
+}  
