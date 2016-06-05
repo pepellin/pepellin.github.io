e6db780e33448e1b32e561b37c1cd0868aee7ec4
pepellin <lin.2049@osu.edu>
2016-06-04 23:19:01 -0400

    Magnific Popup for all images

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,21 +27,17 @@ function initMap() {
 
 // Magnific Popup
 $(document).ready(function() {
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-			// titleSrc: function(item) {
-			// 	return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			// }
-		}
-	});
+	$('.popup-gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      }
+    });
+  });
 });
