ca341c48076a770d80dab17a64d59c006328347d
pepellin <lin.2049@osu.edu>
2016-06-17 01:16:31 -0400

    external json with orderBy popularity

--- moviemovie/assets/app/pmovie-list/pmovie-list.component.js
+++ moviemovie/assets/app/pmovie-list/pmovie-list.component.js
@@ -2,22 +2,11 @@ angular.
   module('pmovieList').
   component('pmovieList', {
     templateUrl: 'assets/app/pmovie-list/phone-list.template.html',
-    controller: function PmovieListController() {
-      this.pmovies = [
-        {
-          title: 'Nexus S',
-          overview: 'Fast just got faster with Nexus S.'
-        }, {
-          title: 'Motorola XOOM™ with Wi-Fi',
-          overview: 'The Next, Next Generation tablet.'
-        }, {
-          title: 'MOTOROLA XOOM™',
-          snioverviewppet: 'The Next, Next Generation tablet.'
-        },
-        {
-          title: 'Nexus S',
-          overview: 'Fast just got faster with Nexus S.'
-        }
-      ];
+    controller: function PmovieListController($http) {
+      var self = this;
+      self.orderProp = '-popularity';
+      $http.get('assets/app/pmovies.json').then(function(response) {
+        self.pmovies = response.data.results.slice(1, 21);
+      });
     }
   });
