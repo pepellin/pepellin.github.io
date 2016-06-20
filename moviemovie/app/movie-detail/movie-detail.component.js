14d9d9518f0b8ebc89898b0136028de1e05a8728
pepellin <lin.2049@osu.edu>
2016-06-20 15:07:32 -0400

    get rid of dummys

--- moviemovie/app/movie-detail/movie-detail.component.js
+++ moviemovie/app/movie-detail/movie-detail.component.js
@@ -19,18 +19,3 @@ angular.
     templateUrl: 'templates/movie-detail.template.1.html',
     controller: ['$routeParams', '$http', MovieDetailController]
   });
-
-//   function NmovieListController($http) {
-//   var self = this;
-//   var movieUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=2d2ec1c3a401007b7c59f97960889607';
-//   $http.get(movieUrl).then(function(response) {
-//     self.movies = response.data.results.slice(0, 12);
-//   });
-// }
-
-// angular.
-//   module('nmovieList').
-//   component('nmovieList', {
-//     templateUrl: 'templates/movie-list.template.2.html',
-//     controller: ['$http', NmovieListController]
-//   });
