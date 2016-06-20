22238b96f0edb02bfb93ff6854c4fa21b84fe027
pepellin <lin.2049@osu.edu>
2016-06-20 12:48:36 -0400

    dynamic reviews and backdrops

--- moviemovie/app/movie-backdrop/movie-backdrop.component.js
+++ moviemovie/app/movie-backdrop/movie-backdrop.component.js
@@ -3,7 +3,7 @@ function MovieBackdropController($routeParams, $http) {
   self.movieId = $routeParams.id;
   var movieUrl = 'https://api.themoviedb.org/3/movie/' + self.movieId +'/images?api_key=2d2ec1c3a401007b7c59f97960889607';
   $http.get(movieUrl).then(function(response) {
-    self.movieBackdrop = response.data;
+    self.movieBackdrop = response.data.backdrops;
   });
 }
 
--- moviemovie/app/movie-review/movie-review.component.js
+++ moviemovie/app/movie-review/movie-review.component.js
@@ -3,7 +3,7 @@ function MovieReviewController($routeParams, $http) {
   self.movieId = $routeParams.id;
   var movieUrl = 'https://api.themoviedb.org/3/movie/' + self.movieId +'/reviews?api_key=2d2ec1c3a401007b7c59f97960889607';
   $http.get(movieUrl).then(function(response) {
-    self.movieReviews = response.data;
+    self.movieReviews = response.data.results;
   });
 }
 
